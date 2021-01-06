6c16dff03d02989034457e404a206f5d61a0655a
Chris DeGroot <cdegroo5@uwo.ca>
2021-01-06 16:20:54 -0500

    Modify app to run on server

--- App.py
+++ App.py
@@ -6,7 +6,8 @@ import plotly.graph_objs as go
 
 import pandas as pd
 
-app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
 
 # Get the data
 url = "https://data.ontario.ca/dataset/f4f86e54-872d-43f8-8a86-3892fd3cb5e6/resource/ed270bb8-340b-41f9-a7c6-e8ef587e6d11/download/covidtesting.csv"
