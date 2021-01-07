5270cb83e9b4bb1de523b2cc3ad0fdbdce055f01
Chris DeGroot <cdegroo5@uwo.ca>
2021-01-07 13:13:16 -0500

    Add death data

--- app.py
+++ app.py
@@ -13,11 +13,18 @@ server = app.server
 
 # Get the data
 url = "https://data.ontario.ca/dataset/f4f86e54-872d-43f8-8a86-3892fd3cb5e6/resource/ed270bb8-340b-41f9-a7c6-e8ef587e6d11/download/covidtesting.csv"
-
 df = pd.read_csv(url)
-df["Daily Cases"] = df["Total Cases"].diff()
-df["7-Day Daily Moving Avg."] = df["Daily Cases"].rolling(7).mean()
+
+# Get the date of last update
 last_updated = df["Reported Date"].iloc[-1]
+
+# Case data calculations
+df["Daily Cases"] = df["Total Cases"].diff()
+df["7-Day Case Avg."] = df["Daily Cases"].rolling(7).mean()
+
+# Death data calculations
+df["Daily Deaths"] = df["Deaths"].diff()
+df["7-Day Death Avg."] = df["Daily Deaths"].rolling(7).mean()
 
 heading_style = {"textAlign" : "center"}
 bar_opacity = 0.6
@@ -33,7 +40,7 @@ daily_cases = get_bar_chart(
 
 daily_cases_avg = go.Scatter(
         x=df["Reported Date"],
-        y=df["7-Day Daily Moving Avg."],
+        y=df["7-Day Case Avg."],
         mode="lines",
         name="7-Day Moving Avg."
     )
@@ -50,21 +57,36 @@ total_cases = get_bar_chart(
         "Total Cases"
     )
 
+daily_deaths = get_bar_chart(
+        df["Reported Date"],
+        df["Daily Deaths"],
+        "Daily Deaths"
+    )
+
+daily_death_avg = go.Scatter(
+        x=df["Reported Date"],
+        y=df["7-Day Death Avg."],
+        mode="lines",
+        name="7-Day Moving Avg."
+    )
+
+total_deaths = get_bar_chart(
+        df["Reported Date"],
+        df["Deaths"],
+        "Total Cases"
+    )
+
 case_layout = go.Layout(
         xaxis={"title" : "Date"},
         yaxis={"title" : "Number of Cases"},
         legend={"x" : 0, "y" : 1}
     )
 
-def get_active_case_plot():
-    return dcc.Graph(
-            id='daily-cases',
-            figure=
-            {
-                'data': [daily_cases, daily_cases_avg],
-                'layout' : case_layout
-            }
-        )
+death_layout = go.Layout(
+        xaxis={"title" : "Date"},
+        yaxis={"title" : "Number of Deaths"},
+        legend={"x" : 0, "y" : 1}
+    )
 
 def get_daily_case_plot():
     return dcc.Graph(
@@ -93,6 +115,26 @@ def get_total_case_plot():
             {
                 'data': [total_cases],
                 'layout' : case_layout
+            }
+        )
+
+def get_daily_death_plot():
+    return dcc.Graph(
+            id='daily-deaths',
+            figure=
+            {
+                'data': [daily_deaths, daily_death_avg],
+                'layout' : death_layout
+            }
+        )
+
+def get_total_death_plot():
+    return dcc.Graph(
+            id='total-deaths',
+            figure=
+            {
+                'data': [total_deaths],
+                'layout' : death_layout
             }
         )
 
@@ -128,6 +170,30 @@ app.layout = dbc.Container(
                 )
             )
         ),
+        html.Br(),
+        dbc.Row(
+            dbc.Col(
+                dbc.Card(
+                    [
+                        dbc.CardHeader(html.H2("Death Data", style=heading_style)),
+                        dbc.CardBody(
+                            [
+                                dbc.Tabs(
+                                    [
+                                        dbc.Tab(label="Daily", tab_id="daily-deaths"),
+                                        dbc.Tab(label="Total", tab_id="total-deaths"),
+                                    ],
+                                    id="death-tabs",
+                                    active_tab="daily-deaths",
+                                ),
+                                html.Div(id="death-tab-content", className="p-4"),
+                            ]
+                        ),
+                    ]
+                )
+            )
+        ),
+        html.Br(),
     ],
     fluid=True
 )
@@ -137,7 +203,7 @@ app.layout = dbc.Container(
     [Input("case-tabs", "active_tab")],
 )
 
-def render_tab_content(active_tab):
+def render_case_tab_content(active_tab):
     """
     This callback takes the 'active_tab' property as input and renders the tab
     content depending on what the value of 'active_tab' is.
@@ -150,6 +216,23 @@ def render_tab_content(active_tab):
         elif active_tab == "total-cases":
             return get_total_case_plot()
     return "No case tab selected"
+
+@app.callback(
+    Output("death-tab-content", "children"),
+    [Input("death-tabs", "active_tab")],
+)
+
+def render_case_tab_content(active_tab):
+    """
+    This callback takes the 'active_tab' property as input and renders the tab
+    content depending on what the value of 'active_tab' is.
+    """
+    if active_tab is not None:
+        if active_tab == "daily-deaths":
+            return get_daily_death_plot()
+        elif active_tab == "total-deaths":
+            return get_total_death_plot()
+    return "No death tab selected"
 
 if __name__ == '__main__':
     app.run_server(debug=False)
