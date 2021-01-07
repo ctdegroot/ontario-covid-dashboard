40afa2ecd1aa43a3172d96ea6c83c434be8e49b2
Chris DeGroot <cdegroo5@uwo.ca>
2021-01-07 11:43:35 -0500

    Add last updated badge

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ url = "https://data.ontario.ca/dataset/f4f86e54-872d-43f8-8a86-3892fd3cb5e6/reso
 df = pd.read_csv(url)
 df["Daily Cases"] = df["Total Cases"].diff()
 df["7-Day Daily Moving Avg."] = df["Daily Cases"].rolling(7).mean()
+last_updated = df["Reported Date"].iloc[-1]
 
 heading_style = {"textAlign" : "center"}
 bar_opacity = 0.6
@@ -50,6 +51,9 @@ total_cases = get_bar_chart(
 
 app.layout = html.Div([
     html.H1("Ontario COVID-19 Data", style=heading_style),
+    html.Div([
+        dbc.Badge("Last updated: {}".format(last_updated), color="primary", className="mr-1")
+    ], className="text-right"),
     dbc.Card([
         dbc.CardHeader(html.H2("Daily Cases", style=heading_style)),
         dbc.CardBody([
